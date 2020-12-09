a2157e32842972bedd2a4e55675b2fdd9aa52c5f
Srinija Dharani <61744464+srinijadharani@users.noreply.github.com>
2020-12-09 10:22:55 +0530

    Add files via upload

---
+++ New folder/Day 01/1.1-BandName.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+place = input("What is the name of the place you grew up in?")
+pet = input("What is the name of your pet?")
+print("Your band name is", place+"", pet)
